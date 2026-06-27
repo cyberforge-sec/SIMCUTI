a30246d3a03991a3af4597c6c4c098189094dfae
cyberforge <adiva@ubuntu.mshome.net>
2026-06-28 02:54:45 +0700

    Berisi skrip untuk membuat struktur tabel 2026_06_27_add_lampiran_url_to_leave_requests.sql

--- database/migrations/2026_06_27_add_lampiran_url_to_leave_requests.sql
+++ database/migrations/2026_06_27_add_lampiran_url_to_leave_requests.sql
@@ -4,4 +4,4 @@ ALTER TABLE public.leave_requests
 ADD COLUMN IF NOT EXISTS lampiran_url TEXT;
 
 COMMENT ON COLUMN public.leave_requests.lampiran_url IS 'Private Supabase Storage path for leave request attachment';
- 
+  
